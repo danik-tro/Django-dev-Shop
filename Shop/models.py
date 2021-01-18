4b4a821c1be0f3156b3186fdef3e7a10a3344ff0
danik-tro <s.danik.tro.c@gmail.com>
2021-01-18 13:47:02 +0200

    Added admin panel

--- Shop/models.py
+++ Shop/models.py
@@ -11,11 +11,15 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50,
-                            verbose_name='Категория')
+    name = models.CharField(max_length=200,
+                            verbose_name='Категория',
+                            db_index=True)
+    slug = models.SlugField(max_length=200, unique=True,
+                            blank=True,
+                            db_index=True)
 
     class Meta:
-        verbose_name = 'Категория'
+        verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
@@ -23,11 +27,24 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50,
-                            verbose_name='Наименование товара')
-    describe = models.TextField(verbose_name='Описание товара')
-    photo = models.ImageField(blank=True, verbose_name='Фото товара')
-    price = models.DecimalField(verbose_name="Цена товара")
+    name = models.CharField(max_length=200,
+                            verbose_name='Наименование продукта',
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            db_index=True)
+    description = models.TextField(verbose_name='Описание продукта',
+                                   blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True,
+                              verbose_name='Изображение продукта')
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name="Цена продукта")
+    stock = models.PositiveIntegerField(verbose_name="Остаток продукта")
+    available = models.BooleanField(default=True, verbose_name='Наличие товара')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     class Meta:
@@ -36,11 +53,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class History(models.Model):
-#     pass
-
-
-# class Cart(models.Model):
-#     pass
--- Shop/urls.py
+++ Shop/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import shop
+from django.conf.urls import url
+from .views import shop, product_list, product_detail
 
 urlpatterns = [
-    path('', shop)
+    path('', shop),
+    url(r'^$', product_list, name='product_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$',
+        product_list,
+        name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        product_detail,
+        name='product_detail'),
 ]
