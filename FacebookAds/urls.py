eb69bc79dd563f2452ed32eab95c3b9ca86db217
danik-tro <s.danik.tro.c@gmail.com>
2021-01-19 11:06:57 +0200

    Created form for order

--- FacebookAds/urls.py
+++ FacebookAds/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^cart', include('cart.urls', namespace='cart')),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^', include('shop.urls', namespace='shop')),
 ]
