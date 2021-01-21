9e12e636787afd52717b862fff0b1425b75c993b
danik-tro <s.danik.tro.c@gmail.com>
2021-01-21 15:36:11 +0200

    Fixed main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,32 @@
-window.onload = function onLoad() {
-    document.getElementById('button_1').onclick = function() {
-        document.getElementById('nav_menu').style.backgroundColor = 'green';
-    };
+colors = {
+    button_1: 'green',
+    button_2: 'red',
+    button_3: 'black'
+}
 
-    document.getElementById('button_2').onclick = function() {
-        document.getElementById('nav_menu').style.backgroundColor = 'red';
-    };
+for (let prop in colors) {
+    document.getElementById(prop).addEventListener('click',
+     function() {
+    document.getElementById('nav_menu').style.backgroundColor = colors[prop];
+    });
+}
 
-    document.getElementById('button_3').onclick = function() {
-        document.getElementById('nav_menu').style.backgroundColor = 'black';
-    };
+let username = document.getElementById('id_username');
 
-    let username = document.getElementById('id_username');
-
-    let elem = document.getElementsByClassName('form-group row')[0]
-        .getElementsByClassName('col-md-9')[0]
-        .appendChild(
+let elem = document.getElementsByClassName('form-group row')[0]
+    .getElementsByClassName('col-md-9')[0]
+    .appendChild(
         document.createElement('p')
-    );
+        );
 
-    function change(value, elem) {
-        elem.style.backgroundColor = "green";
-        elem.innerText = value.replaceAll(/[.,'"\/:;\s\\%|]/g, '');
-    }
+function change(value, elem) {
+    elem.style.backgroundColor = "green";
+    elem.innerText = value.replaceAll(/[.,'"\/:;\s\\%|]/g, '');
+}
 
-    username.onkeyup = function() {
-        let username = document.getElementById('id_username');
-        change(username.value, elem);
-    };
-};
+username.addEventListener('keyup',
+    function() {
+    let username = document.getElementById('id_username');
+    change(username.value, elem);
+    },
+);
